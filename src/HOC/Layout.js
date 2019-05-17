d916a6832a5c24bb3a435bd520344f241b75aff7
Elaine Lee <elaine.lee@ff0000.com>
2019-05-17 15:10:14 -0700

    finish header add footer components

--- src/HOC/Layout.js
+++ src/HOC/Layout.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "../Components/Header_footer/Header";
+import Footer from "../Components/Header_footer/Footer";
 
 const Layout = props => {
-  return <div>{props.children}</div>;
+  return (
+    <div>
+      <Header />
+      {props.children}
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
