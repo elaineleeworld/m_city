4f7fccba93b0db2cdc8eec51ab5f88968cbb524b
Elaine Lee <elaine.lee@ff0000.com>
2019-05-17 17:46:04 -0700

    rotate animation stripes

--- src/Components/home/featured/Stripes.js
+++ src/Components/home/featured/Stripes.js
@@ -4,19 +4,48 @@ import Animate from "react-move/Animate";
 
 class Stripes extends Component {
   state = {
-    stripes: [{ background: "#98c5e9" }, { background: "#ffffff" }, { background: "#98c5e9" }]
+    stripes: [
+      { background: "#98c5e9", left: 120, rotate: 25, top: -260, delay: 0 },
+      { background: "#ffffff", left: 360, rotate: 25, top: -397, delay: 200 },
+      { background: "#98c5e9", left: 600, rotate: 25, top: -498, delay: 400 }
+    ]
   };
   showStripes = () =>
     this.state.stripes.map((stripe, i) => (
       <Animate
         key={i}
         show={true}
-        start={{ background: "#ffffff" }}
-        enter={{ background: [stripe.background], timing: { delay: 500, duration: 200, ease: easePolyOut } }}
+        start={{
+          background: "#ffffff",
+          opacity: 0,
+          left: 0,
+          rotate: 0,
+          top: 0
+        }}
+        enter={{
+          background: [stripe.background],
+          opacity: [1],
+          left: [stripe.left],
+          rotate: [stripe.rotate],
+          top: [stripe.top],
+          timing: { delay: stripe.delay, duration: 200, ease: easePolyOut },
+          events: {
+            end() {
+              console.log("animation finished");
+            }
+          }
+        }}
       >
-        {({ background }) => {
+        {({ opacity, left, rotate, top, background }) => {
           return (
-            <div className="stripe" style={{ background }}>
+            <div
+              className="stripe"
+              style={{
+                background,
+                opacity,
+                transform: `rotate(${rotate}deg) translate(${left}px,${top}px)`
+              }}
+            >
               TEST
             </div>
           );
