bb6e80d181b7688dbdb90a05609f8893592b3ca1
Elaine Lee <elaine.lee@ff0000.com>
2019-05-17 17:16:03 -0700

    add stripes animation

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import Layout from "./HOC/Layout";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./Components/home";
 
 const Routes = prop => {
-  return <Layout>second line</Layout>;
+  return (
+    <Layout>
+      <Switch>
+        <Route exact component={Home} path="/" />
+      </Switch>
+    </Layout>
+  );
 };
 
 export default Routes;
